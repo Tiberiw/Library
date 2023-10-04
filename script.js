c0e8f3946e303b4d92d6939ea501962857257874
Tiberiw <73103485+Tiberiw@users.noreply.github.com>
2023-10-04 12:02:55 +0300

    Basic functionality and css

---
+++ script.js
@@ -0,0 +1,171 @@
+function Book(title, author, pageNumber, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pageNumber = pageNumber;
+    this.isRead = isRead;
+}
+
+Book.prototype.info = function() {
+    return this.title + 
+                " by " + this.author + ", " +
+                this.pageNumber + " pages, " +
+                (this.isRead ? "read" : "not read yet");
+}
+
+Book.prototype.toggleRead = function() {
+    console.log("cacat")
+    this.isRead = !this.isRead;
+}
+
+
+
+
+function Repository() {
+    this.bookRepo = [];
+}
+
+Repository.prototype.addBook = function(title, author, nrPages, read) {
+    const book = new Book(title, author, nrPages, read);
+    this.bookRepo.push(book);
+}
+
+Repository.prototype.getAll = function() {
+    return this.bookRepo;
+}
+
+
+function UI(repo) {
+
+    this.repository = repo;
+
+    this.buttonCloseModal = document.querySelector('button.close-modal');
+    this.buttonShowModal = document.querySelector('button.open-modal');
+    this.modal = document.querySelector('.modal');
+    this.tableBody = document.querySelector('tbody');
+    this.buttonAddBook = document.querySelector('button[type="submit"]');
+    this.inputTitle = document.querySelector('input#title');
+    this.inputAuthor = document.querySelector('input#author');
+    this.inputPages = document.querySelector('input#pages');
+    this.inputRead = document.querySelector('input#read');
+
+    this.setUp = () => {
+        this.buttonAddBook.addEventListener('click', (e) => {
+
+            e.preventDefault();
+            const title = this.inputTitle.value;
+            const author = this.inputAuthor.value;
+            const pages = this.inputPages.value;
+            const read = this.inputRead.checked;
+    
+            this.repository.addBook(title,author,pages,read);
+            this.modal.close();
+            this.showBooks();
+        });
+
+        this.modal.addEventListener('close', () => {
+            this.inputTitle.value = this.inputAuthor.value = this.inputPages.value = "";
+            this.inputRead.checked = false;
+        });
+
+        this.buttonShowModal.addEventListener('click', () => {
+            this.modal.showModal();
+        });
+
+        this.buttonCloseModal.addEventListener('click', () => {
+            this.modal.close();
+        });
+    }
+
+    this.populate = () => {
+        this.repository.addBook("Ceva1", "Branza", 123, false);
+        this.repository.addBook("Ceva2", "Costi", 20, true);
+        this.repository.addBook("Ceva4", "Peleanca", 200, true);
+    }
+
+}
+
+UI.prototype.showBooks = function() {
+
+
+     /* Remove all books to not have duplicates */
+    this.tableBody.innerHTML = "";
+
+
+    /* Iterate on all books from repo */
+    this.repository.bookRepo.forEach( (book,index) => {
+
+        /* Create new table row */
+        const newRow = document.createElement('tr');
+        newRow.setAttribute('data-row',index);
+
+        /* Append title to table */
+        const newTitle = document.createElement('td');
+        newTitle.textContent = book.title;
+        newRow.appendChild(newTitle);
+
+        /* Append author to table */
+        const newAuthor = document.createElement('td');
+        newAuthor.textContent = book.author;
+        newRow.appendChild(newAuthor);
+
+        /* Append pages number to table */
+        const newPages = document.createElement('td');
+        newPages.textContent = book.pageNumber;
+        newRow.appendChild(newPages);
+
+        /* Append status to table */
+        const newRead = document.createElement('td');
+        const toogleReadBtn = document.createElement('button');
+        newRead.appendChild(toogleReadBtn);
+        
+        if(book.isRead === true) {
+            toogleReadBtn.textContent = "READ";
+            toogleReadBtn.classList.add('read');
+            toogleReadBtn.classList.remove('not-read');
+        }
+        else {
+            toogleReadBtn.textContent = "NOT READ";
+            toogleReadBtn.classList.add('not-read');
+            toogleReadBtn.classList.remove('read');
+        }
+            
+
+        toogleReadBtn.addEventListener('click', () => {
+            book.toggleRead();
+            if(book.isRead === true)
+            toogleReadBtn.textContent = "READ";
+        else
+            toogleReadBtn.textContent = "NOT READ";
+            this.showBooks();
+
+        });
+        newRow.appendChild(newRead);
+
+        const newDelete = document.createElement('td');
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete');
+        newDelete.appendChild(deleteBtn);
+
+        deleteBtn.textContent = "DELETE";
+        deleteBtn.addEventListener('click', () => {
+
+            this.tableBody.removeChild(this.tableBody.children[index]);
+            this.repository.bookRepo.splice(index,1);
+            this.showBooks();
+        });
+        newRow.appendChild(newDelete);
+
+        /* Append row to table */
+        this.tableBody.appendChild(newRow);
+
+    });
+
+}
+
+
+
+let Repo = new Repository();
+let GUI = new UI(Repo);
+GUI.setUp();
+GUI.populate();
+GUI.showBooks();
